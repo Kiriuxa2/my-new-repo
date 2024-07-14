939997aae065ea3b67d75819ccabe03e85db7ebd
Игорь Воинов <igorvoinov@MacBook-Air-Igor.local>
2024-07-14 15:36:54 +0300

    переделать

--- bank.py
+++ bank.py
@@ -4,6 +4,7 @@
 ✔ /Допустимые действия: пополнить, снять, выйти
 ✔ /Сумма пополнения и снятия кратны 50 у.е.
 ✔ Процент за снятие — 1.5% от суммы снятия, но не менее 30 и не более 600 у.е.
+
 ✔ После каждой третей операции пополнения или снятия начисляются проценты - 3%
 ✔ Нельзя снять больше, чем на счёте
 ✔ При превышении суммы в 5 млн, вычитать налог на богатство 10% перед каждой
@@ -17,7 +18,7 @@ class Bankomat():
         self.balance = 0  # баланс экземпляра банкомат
         self.quantity = 0
 
-    def cnat(self, money):
+    def cnat(self, money): # не работает
         if self.check1(money) and self.check2(money):
             self.balance -= money
 
@@ -25,25 +26,23 @@ class Bankomat():
         if self.check1(money):
             self.balance += money
 
-        """ разбить один метод проверки на несколько """
     def check1(self, money):
-        # добавить в метод остальные проверки: налоги, кратность и перебор
         return not money % 50
-            
-    def check2(self,money):
+
+    def check2(self, money):
         if money > self.balance:
             return False
-        
-    def check3(self):
+
+    def check3(self): # переделать
         if self.quantity % 3:
-            self.balance += (self.balance / 100 * 3)  # тут не balance
+            self.balance += (self.balance / 100 * 3)
 
     def check4(self):
         if balance >= 5_000_000:
             self.balance -= self.balance * 0.1
 
-    def take_off_percent(self,money):
-        percent = money / 100 * 15  
+    def take_off_percent(self, money):
+        percent = money / 100 * 15
         if percent < 30:
             money -= 30
         elif percent > 600:
@@ -62,10 +61,10 @@ while user_in != 3:
     elif user_in == 2:
         money = int(input('Сумма снятия'))
         balance.cnat(money)
-        Bankomat.take_off_percent(money)
+        # Bankomat.take_off_percent(money) # почему класс?
 
-    balance.quantity += 1  # объект
-    Bankomat.check3()
-    Bankomat.check4()
-    print('Баланс: ', balance)
+    balance.quantity += 1
+    # Bankomat.check3() # почему класс?
+    # Bankomat.check4() # почему класс?
+    print('Баланс: ', balance)  # принт неверный
     user_in = int(input('Команда:(1 - пополнить,2 - снять, 3 - выйти,'))
